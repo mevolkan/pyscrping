73e3de23850f353349a36598b922e365aeec4555
volkan <16579273+mevolkan@users.noreply.github.com>
2023-07-24 22:55:55 +0300

    first commit

---
+++ scrape.py
@@ -0,0 +1,19 @@
+# scrape a website and download data
+# new methods to obtain data from 
+## newer tools and algorithms
+# most efficient
+
+#news summary from different news websites and email to yourself
+# map the location keywords on the map
+
+
+#algorithms
+# geeks for geeks
+
+
+
+#new updates in python
+#get weather data from websites.
+#analyse keywords and compare sentiments
+
+#get alerts from angani.io as push service
